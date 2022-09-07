26c371e04e7c09abca35bfdecb0c4af6352b1c00
Jamie Moreland <jamie1964two@hotmail.co.uk>
2022-09-07 14:11:34 +0100

    staticprops added to dynamic loaded pages

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -13,4 +13,37 @@ function MeetupDetails() {
         />
     );
 }
+export async function getStaticPaths() {
+    return {
+        fallback: false,   // identify whether all path params have been defined
+        paths: [
+            { 
+                params: {
+                meetupid: 'm1'
+                },
+            },
+            { 
+                params: {
+                meetupid: 'm2'
+                },
+            },
+
+        ]
+    }
+}
+export async function getStaticProps(context) {
+    const meetupid = context.params;
+    console.log(meetupid)
+    return {
+        props: {
+            meetupData: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg/1200px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg",
+                id: 'm1',
+                title: "A First Meetup",
+                address: "London Eye",
+                description: "First group get together!"
+            }
+        }
+    }
+}
 export default MeetupDetails;
