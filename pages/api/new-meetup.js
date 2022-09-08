beabed16c74345ed9f302aaf1073c5fc017f8154
Jamie Moreland <jamie1964two@hotmail.co.uk>
2022-09-08 16:19:24 +0100

    added db connectivity

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,19 @@
+// url of this file /api/new-meetup
+// POST requests only
+
+import {MongoClient} from 'mongodb';
+
+async function handler(req, res) {
+    if (req.method === 'POST'){
+        const data = req.body;
+        const client = await MongoClient.connect(process.env.MONGO_DB_CONNECTION);
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+        client.close();
+        res.status(201).json({message: 'New meetup created!'});
+    }
+}
+
+export default handler;
