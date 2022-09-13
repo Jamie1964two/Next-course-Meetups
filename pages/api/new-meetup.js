26ba5961b362c741239acb284ecd2adbace51127
Jamie Moreland <jamie1964two@hotmail.co.uk>
2022-09-13 08:51:36 +0100

    env.local file created

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,7 +6,7 @@ import {MongoClient} from 'mongodb';
 async function handler(req, res) {
     if (req.method === 'POST'){
         const data = req.body;
-        const client = await MongoClient.connect(MONGO_DB_CONNECTION);
+        const client = await MongoClient.connect(process.env.MONGO_DB_CONNECTION);
         const db = client.db();
         const meetupsCollection = db.collection('meetups');
         const result = await meetupsCollection.insertOne(data);
